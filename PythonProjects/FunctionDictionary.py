679c5c1ec8c0c6d32ad9d584343d2609a16c1eb5
Kawaiivee <Kawaiivee@gmail.com>
2018-10-12 23:34:01 -0500

    Adding to function dict

--- PythonProjects/FunctionDictionary.py
+++ PythonProjects/FunctionDictionary.py
@@ -2,12 +2,20 @@ functions = {}
 
 def foo():
     print('foo')
+	
+def fiz():
+	print('fiz')
 
 def baz():
     print('baz')
 
+def bar():
+    print('bar')
+
+functions['b2']=bar
 functions['a']=foo
-functions['b']=baz
+functions['a1']=fiz
+functions['b1']=baz
 
 for fns in functions:
     functions[fns]()
