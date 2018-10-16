725d17167a9ea66752fbcfd9b130251bf1207650
Kawaiivee <Kawaiivee@gmail.com>
2018-10-15 20:52:49 -0500

    We studying neural nets now?!?!?!

--- PythonProjects/BasicNeuralNet.py
+++ PythonProjects/BasicNeuralNet.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env/python3
+
 import numpy as np
 
+#took code from here
+#https://iamtrask.github.io/2015/07/12/basic-python-network/
 # sigmoid function
 def nonlin(x,deriv=False):
     if(deriv==True):
@@ -14,24 +18,25 @@ y = np.array([[0,0,1,1]]).T
 
 # seed random numbers to make calculation
 # deterministic to analyze easier set of results
-np.random.seed(1)
+np.random.seed(2)
 
 # initialize weights randomly with mean 0
 syn0 = 2*np.random.random((3,1)) - 1
-for iter in xrange(10000):
+print("Synapse 0 is:\n", syn0)
+
+generations = 100
+for iter in range(generations):
 # forward propagation
     l0 = X
     l1 = nonlin(np.dot(l0,syn0))
-
 # how much did we miss by?
-l1_error = y - l1
-
-# multiply how much we missed by the
+    l1_error = y - l1
+# multiply how much w  e missed by the
 # slope of the sigmoid at the values in l1
-l1_delta = l1_error * nonlin(l1,True)
-
+    l1_delta = l1_error * nonlin(l1,True)
 # update weights
-syn0 += np.dot(l0.T,l1_delta)
+    syn0 += np.dot(l0.T,l1_delta)
+    #print(l1)
 
-print("Output After Training:")
+print("Output After Training with", generations, "generations:")
 print(l1)
