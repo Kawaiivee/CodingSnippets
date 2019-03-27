07f326aeda013038e8f62ea545de951a93b55d1d
Kawaiivee <Kawaiivee@gmail.com>
2019-03-27 15:51:39 -0500

    Compiled C# for the first time

---
+++ C#Projects/Test.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TestProgram{
+    class Test{
+        static void Main(String[] args){
+            Console.WriteLine("Hello World!");
+            //Console.ReadKey(); <- Simulates a Pause before program terminates
+        }
+    }
+}
